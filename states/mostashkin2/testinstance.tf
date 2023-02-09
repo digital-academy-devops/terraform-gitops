9095922be8655a9b77fd79b7f304c2841b1d12f0
Mikhail Ostashkin <61048189+mostashkin@users.noreply.github.com>
2023-02-09 12:45:56 +0700

    Tf workflow (#3)

---
+++ states/mostashkin2/testinstance.tf
@@ -0,0 +1,43 @@
+locals {
+  zone = "ru-central1-a"
+}
+
+resource "yandex_compute_instance" "testvm" {
+  name        = "testinstance"
+  platform_id = "standard-v1"
+
+  resources {
+    cores         = 2
+    memory        = 2
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.my_image.id
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_network.default.subnet_ids[0] #data.yandex_vpc_subnet.default_zone_subnet.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("yc.pub")}"
+  }
+
+  scheduling_policy {
+    preemptible = true
+  }
+
+}
+
+data "yandex_compute_image" "my_image" {
+  family = "ubuntu-2204-lts"
+}
+
+
+resource "yandex_vpc_network" "default" {
+  name = "default"
+}
