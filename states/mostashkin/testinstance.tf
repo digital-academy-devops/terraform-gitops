5ac7ef5aef9d033874d399ef51642affcb1bbb52
digital-academy-devops <114715641+digital-academy-devops@users.noreply.github.com>
2023-02-16 18:02:09 +0700

    fix(states/mostashkin): use default subnet (#33)
    Co-authored-by: Mikhail Ostashkin <mikhail.ostashkin@invitae.com>

--- states/mostashkin/testinstance.tf
+++ states/mostashkin/testinstance.tf
@@ -1,4 +1,3 @@
-#trigger9
 locals {
   zone = "ru-central1-a"
 }
@@ -20,7 +19,7 @@ resource "yandex_compute_instance" "testvm" {
   }
 
   network_interface {
-    subnet_id = yandex_vpc_subnet.subnet-a.id
+    subnet_id = data.terraform_remote_state.system.outputs.default-subnets[2]
     nat       = true
   }
 
@@ -36,15 +35,4 @@ resource "yandex_compute_instance" "testvm" {
 
 data "yandex_compute_image" "my_image" {
   family = "ubuntu-2204-lts"
-}
-
-
-resource "yandex_vpc_network" "default" {
-  name = "default"
-}
-
-resource "yandex_vpc_subnet" "subnet-a" {
-  v4_cidr_blocks = ["10.2.0.0/16"]
-  zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.default.id
 }
