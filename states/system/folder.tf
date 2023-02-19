32a1267cec7eea7a47c9b2d26fa6dca236b817de
I1ss <lebedewgeorg@yandex.ru>
2023-02-19 20:36:54 +0700

    feat(states/system): add glebedev

--- states/system/folder.tf
+++ states/system/folder.tf
@@ -9,3 +9,15 @@ resource "yandex_resourcemanager_folder" "mostashkin" {
 output "mostashkin-folder-id" {
   value = yandex_resourcemanager_folder.mostashkin.id
 }
+
+resource "yandex_resourcemanager_folder" "glebedev" {
+  cloud_id = data.yandex_resourcemanager_cloud.do-course-1.id
+  name     = "glebedev"
+  labels = {
+    owner = "georgiilebedev"
+  }
+}
+
+output "glebedev-folder-id" {
+  value = yandex_resourcemanager_folder.glebedev.id
+}
