858276f2e7155f3edf953edcd0c36289aaed3350
Vasiliy <sc-2005@yandex.ru>
2023-02-24 13:39:43 +0700

    feat(states/system): add vbystritskiy

--- states/system/folder.tf
+++ states/system/folder.tf
@@ -9,3 +9,15 @@ resource "yandex_resourcemanager_folder" "mostashkin" {
 output "mostashkin-folder-id" {
   value = yandex_resourcemanager_folder.mostashkin.id
 }
+
+resource "yandex_resourcemanager_folder" "vbystritskiy" {
+  cloud_id = data.yandex_resourcemanager_cloud.do-course-1.id
+  name     = "vbystritskiy"
+  labels = {
+    owner = "vasilybystritskiy"
+  }
+}
+
+output "vbystritskiy-folder-id" {
+  value = yandex_resourcemanager_folder.vbystritskiy.id
+}
