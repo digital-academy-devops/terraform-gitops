3d39ba9d39a5e68e7931da9441a30ba2651a8353
Andrew Robonen <robonenandrew@gmail.com>
2023-02-25 15:13:11 +0700

    feat(states/system): add robonen folder (#40)

--- states/system/folder.tf
+++ states/system/folder.tf
@@ -9,3 +9,15 @@ resource "yandex_resourcemanager_folder" "mostashkin" {
 output "mostashkin-folder-id" {
   value = yandex_resourcemanager_folder.mostashkin.id
 }
+
+resource "yandex_resourcemanager_folder" "robonen" {
+  cloud_id = data.yandex_resourcemanager_cloud.do-course-1.id
+  name     = "robonen"
+  labels = {
+    owner = "andrewrobonen"
+  }
+}
+
+output "robonen-folder-id" {
+  value = yandex_resourcemanager_folder.robonen.id
+}
