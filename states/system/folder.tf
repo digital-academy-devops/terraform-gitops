4a19fb99e3de9e294e84cab789a01ad26269f7a4
Anton Malkov <51389727+Sodx1@users.noreply.github.com>
2023-03-02 18:38:27 +0700

    fix folder.tf

--- states/system/folder.tf
+++ states/system/folder.tf
@@ -10,9 +10,6 @@ output "mostashkin-folder-id" {
   value = yandex_resourcemanager_folder.mostashkin.id
 }
 
-
-
-
 resource "yandex_resourcemanager_folder" "glebedev" {
   cloud_id = data.yandex_resourcemanager_cloud.do-course-1.id
   name     = "glebedev"
