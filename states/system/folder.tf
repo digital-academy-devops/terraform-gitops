5fbe15696b50f9c186a0b717509da6caf1eebb3b
Anton Malkov <anthonyprorok@hotmail.com>
2023-03-02 18:30:56 +0700

    back changes

--- states/system/folder.tf
+++ states/system/folder.tf
@@ -10,6 +10,33 @@ output "mostashkin-folder-id" {
   value = yandex_resourcemanager_folder.mostashkin.id
 }
 
+
+
+
+resource "yandex_resourcemanager_folder" "glebedev" {
+  cloud_id = data.yandex_resourcemanager_cloud.do-course-1.id
+  name     = "glebedev"
+  labels = {
+    owner = "lebedevgeorgii"
+  }
+}
+
+output "glebedev-folder-id" {
+  value = yandex_resourcemanager_folder.glebedev.id
+}
+
+resource "yandex_resourcemanager_folder" "robonen" {
+  cloud_id = data.yandex_resourcemanager_cloud.do-course-1.id
+  name     = "robonen"
+  labels = {
+    owner = "andrewrobonen"
+  }
+}
+
+output "robonen-folder-id" {
+  value = yandex_resourcemanager_folder.robonen.id
+}
+
 resource "yandex_resourcemanager_folder" "amalkov" {
   cloud_id = data.yandex_resourcemanager_cloud.do-course-1.id
   name     = "amalkov"
@@ -21,5 +48,3 @@ resource "yandex_resourcemanager_folder" "amalkov" {
 output "amalkov-folder-id" {
   value = yandex_resourcemanager_folder.amalkov.id
 }
-
-
