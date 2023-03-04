478272085f058f54c11f82f94562360b928bd065
TeqquilaSunset <86839875+TeqquilaSunset@users.noreply.github.com>
2023-03-04 23:46:59 +0700

    feat(states/system): add vbystritskiy (#39)

--- states/system/folder.tf
+++ states/system/folder.tf
@@ -45,3 +45,15 @@ resource "yandex_resourcemanager_folder" "amalkov" {
 output "amalkov-folder-id" {
   value = yandex_resourcemanager_folder.amalkov.id
 }
+
+resource "yandex_resourcemanager_folder" "vbystritskiy" {
+  cloud_id = data.yandex_resourcemanager_cloud.do-course-1.id
+  name     = "vbystritskiy"
+  labels = {
+    owner = "vasilybystritskiy"
+  }
+}
+
+output "vbystritskiy-folder-id" {
+  value = yandex_resourcemanager_folder.vbystritskiy.id
+}
