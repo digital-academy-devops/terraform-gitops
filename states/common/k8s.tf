6b064358ab2cec4fb1211664aeae85b470ac2108
Mikhail Ostashkin <mikhail.ostashkin@invitae.com>
2023-04-04 16:45:54 +0700

    feat(system/common): create k8s

---
+++ states/common/k8s.tf
@@ -0,0 +1,122 @@
+data "yandex_vpc_network" "default" {
+  network_id = "enp4avdfrvnj4hjf2aih"
+}
+
+data "yandex_vpc_subnet" "default" {
+  count = length(data.yandex_vpc_network.default.subnet_ids)
+  subnet_id = data.yandex_vpc_network.default.subnet_ids[count.index]
+}
+
+locals {
+  folder_id   = data.terraform_remote_state.system.outputs.common-folder-id
+  version = "1.23"
+  sa_name     = "kube_sa"
+}
+
+resource "yandex_kubernetes_cluster" "k8s-regional" {
+  network_id = data.yandex_vpc_network.default.id
+  master {
+    version = local.version
+
+    regional {
+      region = "ru-central1"
+
+      dynamic "location" {
+        for_each = data.yandex_vpc_subnet.default.*
+        content {
+          zone      = location.value.zone
+          subnet_id = location.value.subnet_id
+        }
+      }
+    }
+
+    security_group_ids = [yandex_vpc_security_group.k8s-main-sg.id]
+  }
+  service_account_id      = yandex_iam_service_account.k8s.id
+  node_service_account_id = yandex_iam_service_account.k8s.id
+
+  depends_on = [
+    yandex_resourcemanager_folder_iam_member.k8s-clusters-agent,
+    yandex_resourcemanager_folder_iam_member.vpc-public-admin,
+    yandex_resourcemanager_folder_iam_member.images-puller
+  ]
+
+  kms_provider {
+    key_id = yandex_kms_symmetric_key.kms-key.id
+  }
+}
+
+resource "yandex_iam_service_account" "k8s" {
+  name        = local.sa_name
+  description = "K8S regional service account"
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "k8s-clusters-agent" {
+  folder_id = local.folder_id
+  role      = "k8s.clusters.agent"
+  member    = "serviceAccount:${yandex_iam_service_account.k8s.id}"
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "vpc-public-admin" {
+  folder_id = local.folder_id
+  role      = "vpc.publicAdmin"
+  member    = "serviceAccount:${yandex_iam_service_account.k8s.id}"
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "images-puller" {
+  folder_id = local.folder_id
+  role      = "container-registry.images.puller"
+  member    = "serviceAccount:${yandex_iam_service_account.k8s.id}"
+}
+
+resource "yandex_kms_symmetric_key" "kms-key" {
+  name              = "kms-key"
+  default_algorithm = "AES_128"
+  rotation_period   = "8760h" # 1 year
+}
+
+resource "yandex_kms_symmetric_key_iam_binding" "viewer" {
+  symmetric_key_id = yandex_kms_symmetric_key.kms-key.id
+  role             = "viewer"
+  members = [
+    "serviceAccount:${yandex_iam_service_account.k8s.id}",
+  ]
+}
+
+resource "yandex_vpc_security_group" "k8s-main-sg" {
+  name        = "k8s-main-sg"
+  network_id  = data.yandex_vpc_network.default.id
+  ingress {
+    protocol          = "TCP"
+    description       = "Allow healtchecks"
+    predefined_target = "loadbalancer_healthchecks"
+    from_port         = 0
+    to_port           = 65535
+  }
+  ingress {
+    protocol          = "ANY"
+    description       = "Allow internal communication"
+    predefined_target = "self_security_group"
+    from_port         = 0
+    to_port           = 65535
+  }
+  ingress {
+    protocol          = "ICMP"
+    description       = "allow ICMP"
+    v4_cidr_blocks    = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+  }
+  ingress {
+    protocol          = "TCP"
+    description       = "allow incoming nodeports connections"
+    v4_cidr_blocks    = ["0.0.0.0/0"]
+    from_port         = 30000
+    to_port           = 32767
+  }
+  egress {
+    protocol          = "ANY"
+    description       = "allow all"
+    v4_cidr_blocks    = ["0.0.0.0/0"]
+    from_port         = 0
+    to_port           = 65535
+  }
+}
