63afeccd6248a06dbfff99a76f8de968e435950f
digital-academy-devops <114715641+digital-academy-devops@users.noreply.github.com>
2023-04-05 16:49:19 +0700

    Revert "feat(states/system): wide roles (#78)" (#79)
    This reverts commit 53c0d8ab646b3e69c27d5d3c510c6408fdda1f65.

--- states/system/sa.tf
+++ states/system/sa.tf
@@ -4,7 +4,14 @@ module "terraform-sa" {
   folder_id = data.yandex_resourcemanager_folder.do-course-1-default.id
   cloud_id  = data.yandex_resourcemanager_cloud.do-course-1.id
   roles = [
-    "admin"
+    "compute.admin",
+    "resource-manager.admin",
+    "iam.admin",
+    "storage.editor",
+    "vpc.admin",
+    "kms.admin",
+    "k8s.admin",
+    "k8s.cluster-api.cluster-admin",
   ]
 }
 
@@ -14,6 +21,14 @@ module "terraform-viewer-sa" {
   folder_id = data.yandex_resourcemanager_folder.do-course-1-default.id
   cloud_id  = data.yandex_resourcemanager_cloud.do-course-1.id
   roles = [
-    "viewer"
+    "compute.viewer",
+    "resource-manager.viewer",
+    "storage.viewer",
+    "iam.viewer",
+    "vpc.viewer",
+    "load-balancer.viewer",
+    "kms.viewer",
+    "k8s.viewer",
+    "k8s.cluster-api.viewer",
   ]
 }
