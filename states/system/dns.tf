3cb2357a39b020555e503a84e894d0afb5dd0996
Mikhail Ostashkin <mikhail.ostashkin@invitae.com>
2023-04-06 08:35:46 +0700

    fix(states/system): dns folder

--- states/system/dns.tf
+++ states/system/dns.tf
@@ -1,5 +1,10 @@
+locals {
+  default_folder_id = "b1gd1rrp2lk1onr8rfhk"
+}
+
 resource "yandex_dns_zone" "dacdevops" {
   name = "dacdevops"
+  folder_id = local.default_folder_id
 
   zone   = "dacdevops.ru."
   public = true
