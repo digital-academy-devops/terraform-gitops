df7609a7de0ffa8edbb5e6d714f9a48850a397e3
Herman7883 <aaagrm6@gmail.com>
2023-05-15 17:47:29 +0700

    5

--- states/system/folder.tf
+++ states/system/folder.tf
@@ -38,7 +38,7 @@ resource "yandex_resourcemanager_folder" "Herman7883 {
   cloud_id = data.yandex_resourcemanager_cloud.do-course-1.id
   name     = "Herman7883"
   labels = {
-    owner = "Herman7883"
+    owner = "hermanzh"
   }
 }
 
