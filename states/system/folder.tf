45edc8a814b55dfd8fb8ac082dfc6de668319bff
mikys997 <melekhov206@gmail.com>
2023-06-01 14:52:17 +0700

    (change owner)

--- states/system/folder.tf
+++ states/system/folder.tf
@@ -192,7 +192,7 @@ resource "yandex_resourcemanager_folder" "nmelekhov" {
   cloud_id = data.yandex_resourcemanager_cloud.do-course-1.id
   name     = "nmelekhov"
   labels = {
-    owner = "nmelekhov"
+    owner = "nikitamelekhov"
   }
 }
 
